22ae0e2b6004856f91729337b543de14725c1314
nguonhour <hengnguonhour@gmail.com>
2024-11-03 17:18:35 +0700

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,6 @@
+function changecolor(color){
+    var getcolor = document.querySelector("#box");
+    getcolor.style.backgroundColor = color;
+    getcolor.style.width = "300px";
+    getcolor.style.height = "300px";
+}
